00919dfca8faf8e48919d630a6b817f59fe98864
Yordi Ogando <yordiogando@gmail.com>
2020-10-06 23:22:27 -0400

    Client initial stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publickey =
+    "pk_test_51GxDYJF2UTDM6hXNBFbtzHJ4tA0Eq4csKzY4bQNGBlDy5Ue7opzA8YAojHmZB63xrhcH7h3bVSNfzx1ym8Rgjpm300Z2xKk7Kz";
+
+  const onToken = (token) => {
+    console.log(token);
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Clothing Ecommerce"
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      token={onToken}
+      stripeKey={publickey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
