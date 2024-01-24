3354376f5d3027cdb51fb7cdfb7ba7f37435d6ed
Yordi Ogando <yordiogando@gmail.com>
2024-01-23 21:12:20 -0400

    Updates

---
+++ precache-manifest.8fcc902a7e5a29da85b8f0ab1b402cf7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2827d00908784219d0f29783cf98ccb9",
+    "url": "/Clothing_Ecommerce/index.html"
+  },
+  {
+    "revision": "2d1800882fa236ccf133",
+    "url": "/Clothing_Ecommerce/static/css/main.a6bbd7a5.chunk.css"
+  },
+  {
+    "revision": "6616e05e4bf89639d059",
+    "url": "/Clothing_Ecommerce/static/js/2.d88becc5.chunk.js"
+  },
+  {
+    "revision": "d7e164bc880d25710db675f58f847190",
+    "url": "/Clothing_Ecommerce/static/js/2.d88becc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d1800882fa236ccf133",
+    "url": "/Clothing_Ecommerce/static/js/main.6e2623c0.chunk.js"
+  },
+  {
+    "revision": "206d53ce3737174695fd",
+    "url": "/Clothing_Ecommerce/static/js/runtime-main.e1923cb4.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/Clothing_Ecommerce/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/Clothing_Ecommerce/static/media/shopping-bag.30f07071.svg"
+  }
+]);
