40045c7f911d9b6f54ab910c2caa17d748cc8002
Yordi Ogando <yordiogando@gmail.com>
2024-01-23 21:14:23 -0400

    test

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -31,7 +31,7 @@ const Header = ({ currentUser, hidden }) => (
         </div>
       ) : (
         <Link className="option" to="/signin">
-          SIGN IN
+          SIGN-IN
         </Link>
       )}
       <CartIcon />
